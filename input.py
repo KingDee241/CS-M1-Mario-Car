c89f854073019f57b2f2e5dabe95acf0f6814f86
Bragol_ <57509042+KingDee241@users.noreply.github.com>
2021-11-15 10:44:25 -0500

    Switch to Pynput Test

--- input.py
+++ input.py
@@ -1,21 +1,38 @@
-import keyboard
+from pynput import keyboard
 # import car
 # from car.motors import set_steering
 
-def get_vertical():
-    input = keyboard.read_key()
-    if input == "W":
-        return 20
-    elif input == "S":
-        return -20
-    else:
-        return 0
 
-def get_horizontal():
-    input = keyboard.read_key()
-    if input == "A":
-        return -0.1
-    elif input == "D":
-        return 0.1
-    else:
-        return 0
+def on_press(key):
+    try:
+        print('alphanumeric key {0} pressed'.format(
+            key.char))
+    except AttributeError:
+        print('special key {0} pressed'.format(
+            key))
+
+
+def on_release(key):
+    print('{0} released'.format(
+        key))
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+# def get_vertical():
+#     input = keyboard.read_key()
+#     if input == "W":
+#         return 20
+#     elif input == "S":
+#         return -20
+#     else:
+#         return 0
+#
+# def get_horizontal():
+#     input = keyboard.read_key()
+#     if input == "A":
+#         return -0.1
+#     elif input == "D":
+#         return 0.1
+#     else:
+#         return 0
