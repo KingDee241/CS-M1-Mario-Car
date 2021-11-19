bf44bcdc49c3829213a254d931d172f350ce0c05
Saph <54683098+sswangg@users.noreply.github.com>
2021-11-19 10:55:07 -0500

    Update keyboard_control.py

--- keyboard_control.py
+++ keyboard_control.py
@@ -3,18 +3,22 @@ import car
 from car.motors import set_throttle, set_steering
 
 key_to_speed = {'w': 20, 's': -20}
-key_to_speed_caps = {'w': 40, 's': -40}
+key_to_speed_caps = {'w': 30, 's': -30}
 key_to_steer = {'a': +45, 'd': -45}
 
+speedy = False
+
 def on_press(key):
+    global speedy
+    if key == keyboard.Key.caps_lock:
+        speedy = not speedy
     try:
         key_char = key.char.lower()
     except:
         return
     speed_dict = key_to_speed
-    if keyboard.Controller.shift_pressed:
+    if speedy:
         speed_dict = key_to_speed_caps
-    
     if key_char in key_to_speed:
         set_throttle(speed_dict[key_char])
     elif key_char in key_to_steer:
@@ -32,12 +36,14 @@ def on_release(key):
     print('{0} released'.format(
         key))
     try:
-        key.char
+        key_char = key.char.lower()
     except:
         return
-    if key.char in key_to_speed:
+    
+        
+    if key_char in key_to_speed:
         set_throttle(0)
-    if key.char in key_to_steer:
+    if key_char in key_to_steer:
         set_steering(0)
     if key == keyboard.Key.esc:
         # Stop listener
