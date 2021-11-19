eb5e581720fdc160a3661fa25b7a0f4ead45b078
Saph <54683098+sswangg@users.noreply.github.com>
2021-11-19 10:23:15 -0500

    Input Created

--- keyboard_control.py
+++ keyboard_control.py
@@ -1,10 +1,15 @@
 from pynput import keyboard
 import car
+from car.motors import set_throttle, set_steering
 
+key_to_speed = {'w': 20, 's': 20}
+key_to_steer = {'a': 45, 'd': -45}
 
 def on_press(key):
-    if key.char == 'w':
-        car.forward(0.5)
+    if key.char in key_to_speed:
+        car.set_throttle(key_to_speed[key.char])
+    if key.char in key_to_steer:
+        car.set_steering(key_to_steer[key.char])
     try:
         print('alphanumeric key {0} pressed'.format(
             key.char))
@@ -15,6 +20,10 @@ def on_press(key):
 def on_release(key):
     print('{0} released'.format(
         key))
+    if key.char in key_to_speed:
+        car.set_throttle(0)
+    if key.char in key_to_steer:
+        car.set_steering(0)
     if key == keyboard.Key.esc:
         # Stop listener
         return False
